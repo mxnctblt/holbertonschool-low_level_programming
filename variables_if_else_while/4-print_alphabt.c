b3c1f1de7c910f44c229cfd95dec3b5dd5c198ac
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-03 05:13:47 -0700

    task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * *main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+		char ch = 'a';
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			if (ch != 'q' && ch != 'e')
+			{
+				putchar(ch);
+			}
+		}
+		putchar('\n');
+		return (0);
+}
