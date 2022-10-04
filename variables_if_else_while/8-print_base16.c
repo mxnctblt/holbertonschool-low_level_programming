ba8defe886938826ac29e51823e90a519273e698
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-04 00:38:26 -0700

    task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* *main - Entry point
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+		int num;
+		char alph = 'a';
+
+		for (num = 0; num < 10; num++)
+		{
+			putchar((num % 10) + '0');
+		}
+		for (alph = 'a'; alph <= 'f'; alph++)
+		{
+			putchar(alph);
+		}
+			putchar('\n');
+		return (0);
+}
