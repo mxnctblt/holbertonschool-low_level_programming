efa6f53b985ea79479deb27508ee7bbb0c8517b2
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-04 00:22:16 -0700

    task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* *main - Entry point
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+		char ch = 'z';
+
+		for (ch = 'z'; ch >= 'a'; ch--)
+		{
+			putchar(ch);
+		}
+			putchar('\n');
+		return (0);
+}
