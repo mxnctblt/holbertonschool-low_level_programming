c1f1c214c619666caf45f8b39cff552798780e8c
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-05 01:14:20 -0700

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alphabet - function
+*
+* Return: nothing.
+*/
+void print_alphabet(void)
+{
+	int l = 'a';
+
+	while (l <= 'z')
+	{
+		_putchar(l);
+		1 += 1;
+	}
+	_putchar('\n');
+}
