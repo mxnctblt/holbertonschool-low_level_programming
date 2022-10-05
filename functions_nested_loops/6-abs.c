39c183dc1b235837ee53e44bcba31d06c4f07013
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-05 04:47:45 -0700

    task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ *
+ *@x: description of x
+ *
+ * Return: 0.
+ */
+int _abs(int x);
+{
+	if (x <= 0)
+	{
+		return(x = x * -1);
+
+	}
+		return (0);
+}
