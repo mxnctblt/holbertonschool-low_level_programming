be5b40d247e37c181cb774fc8f3327de0a4e6f25
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-05 05:35:24 -0700

    task 7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -3,10 +3,30 @@
 /**
 * print_last_digit - Entry point
 *
-*@x: description of x
+*@l: description of l
 *
 * Return: 0.
 */
-int print_last_digit(int x)
+int print_last_digit(int l)
 {
-
+	if (l > 0)
+	{
+		l = l % 10;
+		_putchar('0' + l);
+	}
+	else if (l < 0)
+	{
+		l = l % 10 * -1;
+		_putchar('0' + l);
+	}
+	else if (l == 0)
+	{
+		l = 0;
+		_putchar('0' + l);
+	}
+	else
+	{
+		return (0);
+	}
+	return (l);
+}
