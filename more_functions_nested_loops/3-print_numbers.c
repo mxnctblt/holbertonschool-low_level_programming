9bcfa8ecf77622d61a6679a576e0144b9e1c5434
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-07 01:04:56 -0700

    task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
