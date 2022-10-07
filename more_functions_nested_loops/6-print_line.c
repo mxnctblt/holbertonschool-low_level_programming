83ef14675c1c46b7b724baea6b576371a4cf2753
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-07 02:44:34 -0700

    task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * @n: n
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
