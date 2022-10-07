6111bc0a08bb5eb299c73076f7ef31adb1b5e1d1
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-07 00:42:51 -0700

    task 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit  - Entry point
+ * @c: c
+ * Return: Always 0.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
