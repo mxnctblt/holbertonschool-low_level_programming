aa2c7cb2b87dbe18ad5315000d38edca93ecc3a5
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-07 01:18:34 -0700

    task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		if (n != 50 && n != 52)
+		{
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+}
