cfc6ba1297e56aefcafb1b685e10ba2bb40f3074
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-11 04:51:54 -0700

    task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * print_rev - entry point
@@ -10,12 +9,14 @@
  */
 void print_rev(char *s)
 {
-	int len = strlen(s);
+	int len = 0;
 	int i;
 
-	for (i = len - 1; 1 >= 0; i--)
-	{
+	while (s[i++])
+		len++;
+
+	for (i = len - 1; i >= 0; i--)
 		_putchar(s[i]);
-	}
+
 	_putchar('\n');
 }
