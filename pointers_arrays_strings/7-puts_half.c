0180236bcfba3440454224f389cf4193a11ee7dc
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-11 06:43:32 -0700

    task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - entry point
+ * @str: string
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+
+	int i = 0, len = 0, n;
+
+	while (str[i++])
+		len++;
+
+	if ((len % 2) == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
