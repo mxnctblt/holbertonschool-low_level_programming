15d72ac5b9df69fd30cbd61a8e8b5802f2b1dd35
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-11 05:56:38 -0700

    task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - entry point
+ * @str: string
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	while (*str)
+	{
+		if (*str % 2 == 0)
+		{
+			_putchar(*str);
+		}
+		str++;
+	}
+	_putchar('\n');
+}
