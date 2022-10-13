59bebbe8501234717ce05c8f2af667b144c8fd30
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-13 06:54:07 -0700

    task 5.1

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - entry point
+ * @str: string
+ * Return: str.
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
