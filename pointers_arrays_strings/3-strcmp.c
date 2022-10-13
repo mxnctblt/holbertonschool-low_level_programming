ae5f4f83ae46fba37216829973c2b93dc84367c2
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-13 06:13:03 -0700

    task 3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strcmp - entry point
+ * @s1: string 1
+ * @s2: string 2
+ * Return: void.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	for (; (*s1 != '\0' && *s2 != '\0') && (*s1 == *s2); s1++, s2++)
+		;
+	if (*s1 == *s2)
+		return (0);
+	return (*s1 - *s2);
+}
