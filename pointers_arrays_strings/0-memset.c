14700ad6d1af818e6620e80fc39b9c0625204a85
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-14 01:07:06 -0700

    task 0.2

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - entry point
+ * @s: s
+ * @b: b
+ * @n: n
+ * Return: void.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char* p = s;
+
+	while(n--)
+	{
+		*p++ = (char)b;
+	}
+	return (s);
+}
