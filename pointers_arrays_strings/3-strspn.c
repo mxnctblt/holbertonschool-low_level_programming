72f926a15659de85107a3b1834ad087431c93601
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-14 01:52:39 -0700

    task 3.1

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strspn - entry point
+ * @accept: accept
+ * @s: s
+ * Return: void.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int len = 0;
+
+	if ((s == NULL) || (accept == NULL))
+	{
+		return (len);
+	}
+	while (*s && strchr(accept, *s++))
+	{
+		len++;
+	}
+	return (len);
+}
