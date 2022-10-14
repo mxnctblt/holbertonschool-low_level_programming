84c97904a3678d928ec8491a7f4a1723370b31e2
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-14 05:57:16 -0700

    task 5.2

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strstr - entry point
+ * @haystack: haystack
+ * @needle: needle
+ * Return: void.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	if (needle[0] == '\0')
+		return (haystack);
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+					break;
+			}
+			if (needle[j] == '\0')
+				return (haystack + i);
+		}
+	}
+	return ('\0');
+}
