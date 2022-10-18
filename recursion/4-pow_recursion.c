14dab376984b12d92f3fc4a22754fcd980b5fff8
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-18 05:09:27 -0700

    task 4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - entry point
+ * @x: x
+ * @y: y
+ * Return: -1 if y < 0.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
+	else if (y < 0)
+		return (-1);
+	else
+		return (1);
+}
