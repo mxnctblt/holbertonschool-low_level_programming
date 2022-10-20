94928135b9415e89048a7a061b647c92b18423ad
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-20 07:02:28 -0700

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arg
+ * @argv: argv
+ * Return: 0.
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
