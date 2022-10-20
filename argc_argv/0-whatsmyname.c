f2085400c1da958fb5bfe0a690b58df34b715563
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-20 06:37:30 -0700

    task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: argc
+ * @argv: argv
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,11 @@
+C - argc, argv
+
+File: 0-whatsmyname.c
+
+File: 1-args.c
+
+File: 2-args.c
+
+File: 3-mul.c
+
+File: 4-add.c
