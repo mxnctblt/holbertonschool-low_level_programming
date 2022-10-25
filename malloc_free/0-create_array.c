d881c92287ef1528d3645c6bf2d32b6f12d50e17
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-25 07:00:41 -0700

    task O

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	char *ar;
 
 	ar = malloc(size * sizeof(char));
-	if (size == 0 || array == NULL)
+	if (size == 0 || ar == NULL)
 		return (NULL);
 	while (i < size)
 	{
