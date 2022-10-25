8ef228d801b38a12a789590fa1cdd9d1b8760371
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-25 12:17:36 -0700

    task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,8 +14,10 @@ char *str_concat(char *s1, char *s2)
 	int size1, size2;
 	char *ar;
 
-	if (s1 == NULL || s2 == NULL)
-		s1 = s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (size1 <= *s1)
 		size1++;
 	while (size2 <= *s2)
