16e985604543ad27a55083a5a87520f7e465e988
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-27 03:44:36 -0700

    task 3

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	ar = malloc(sizeof(int) * (max-min) + 1);
+	ar = malloc(sizeof(int) * (max - min) + sizeof(int));
 	if (ar == NULL)
 		return (NULL);
 	for (i = min; i <= max; i++)
