f5c1835da5474d7c6d390078c52dfe83fec6676c
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-31 08:29:54 -0700

    task 3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,8 +14,12 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+/**
+ * struct dog_t - typedef of dog_t
+ */
+typedef struct dog dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 #endif
