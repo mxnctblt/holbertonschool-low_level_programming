2e53613ecb870bdf0e501e4c754833b0c79d3d59
mxnctblt <thibaultmaxence44@gmail.com>
2022-10-31 05:49:32 -0700

    task 0

---
+++ structures_typedef/README.md
@@ -0,0 +1,11 @@
+C - Structures, typedef
+
+File: dog.h
+
+File: 1-init_dog.c
+
+File: 2-print_dog.c
+
+File: 4-new_dog.c
+
+File: 5-free_dog.c
---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ * struct my_struct - Short description
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
