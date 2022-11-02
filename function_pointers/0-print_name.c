d51b12f3889943acbae2ed203295368eb5226884
mxnctblt <thibaultmaxence44@gmail.com>
2022-11-02 12:58:36 -0700

    task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that print name
+ * @f: founction pointer
+ * @name: name to print
+ * Return: 0.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ function_pointers/README.md
@@ -0,0 +1,9 @@
+C - Function pointers
+
+File: 0-print_name.c
+
+File: 1-array_iterator.c
+
+File: 2-int_index.c
+
+File: 3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h
