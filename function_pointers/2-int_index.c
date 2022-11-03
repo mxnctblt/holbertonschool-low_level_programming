2e6b5b03f41825546a9812eccbcbd44b49dc802a
mxnctblt <thibaultmaxence44@gmail.com>
2022-11-03 04:19:14 -0700

    task 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searches for an integer
+ * @array: the array to check
+ * @size: the size of the array
+ * @cmp: pointer to the function
+ * Return: -1 or i.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]))
+			return (i);
+	return (0);
+}
