ffd27eebc02ca8c0c26425b97084d8a454a344d7
mxnctblt <thibaultmaxence44@gmail.com>
2022-11-14 05:55:53 -0800

    task 2, fix description

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -4,8 +4,8 @@
 
 /**
  * add_node -  function that adds a new node at the beginning of a list_t list.
- * @head: pointer functionY
- * @str: the duplicated string to print
+ * @head: pointer function
+ * @str: the string to be duplicated
  * Return: the address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
