07e19a0a145bd1f00223e968f426a263f42810a3
mxnctblt <thibaultmaxence44@gmail.com>
2022-11-16 06:39:53 -0800

    task 9

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - function that inserts a new node at a given positi
+ * @head: node
+ * @idx: index of the list where the new node should be added.
+ * @n: new data
+ * Return: the address of the new node, or NULL if it failed
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *cpy = *head;
+	unsigned int i = 0;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL || idx == 0)
+		return (NULL);
+
+	new->n = n;
+
+	while (i < idx - 1)
+	{
+		if (cpy == NULL)
+			return (NULL);
+		cpy = cpy->next;
+		i++;
+	}
+	new->next = cpy->next;
+	cpy->next = new;
+
+	return (new);
+}
