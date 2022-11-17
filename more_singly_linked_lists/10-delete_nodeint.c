b3cf4dceb4177e67c91f94c3b56587533b73312a
mxnctblt <thibaultmaxence44@gmail.com>
2022-11-17 11:50:59 -0800

    task 10

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - function that deletes the node at index
+ * @head: node
+ * @idx: index of the list where the new node should be added.
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *new, *cpy = *head;
+	unsigned int i = 0;
+
+	if (cpy == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(cpy);
+		return (1);
+	}
+	for (i = 0; i < index - 1; i++)
+	{
+		if (cpy->next == NULL)
+			return (-1);
+		cpy = cpy->next;
+	}
+
+	new = cpy->next;
+	cpy->next = new->next;
+	free(new);
+
+	return (1);
+}
