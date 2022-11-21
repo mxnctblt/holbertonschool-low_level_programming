fe0c04be1039c66fbd5c548d376142da90a32a07
mxnctblt <thibaultmaxence44@gmail.com>
2022-11-21 14:44:11 -0800

    task 0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a text file and prints it to the POSIX standard output.
+ * @filename: pointer to the name of the file
+ * @letters: the number of letters it should read and print
+ * Return: the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
+}
---
+++ file_io/README.md
@@ -0,0 +1,9 @@
+## C - File I/O
+
+File: 0-read_textfile.c - function that reads a text file and prints it to the POSIX standard output.
+
+File: 1-create_file.c - function that creates a file.
+
+File: 2-append_text_to_file.c - function that appends text at the end of a file.
+
+File: 3-cp.c - a program that copies the content of a file to another file.
