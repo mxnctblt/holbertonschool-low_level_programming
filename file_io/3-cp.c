a844acf3b631150aa6c06d36fc2bf46262842abc
mxnctblt <thibaultmaxence44@gmail.com>
2022-11-22 00:22:57 -0800

    task 3 test2

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,52 +9,41 @@
 int main(int argc, char *argv[])
 {
 	int file_from, file_to;
-	int r = 1024, w = 0;
-	char buffer[1024];
+	int num1 = 1024, num2 = 0;
+	char buf[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY | O_APPEND);
+	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY | O_APPEND, 0664);
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(file_from), exit(99);
 	}
-
-	while (r == 1024)
+	while (num1 == 1024)
 	{
-		r = read(file_from, buffer, 1024);
+		num1 = read(file_from, buf, 1024);
+		if (num1 == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-		w = write(file_to, buffer, r);
-		if (w < r)
+		num2 = write(file_to, buf, num1);
+		if (num2 < num1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 
 	if (close(file_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 
 	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 
 	return (0);
 }
