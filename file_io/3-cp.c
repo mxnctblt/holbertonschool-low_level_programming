2f94b41def8e7db40a43f7ec520ab47a0e80dfe6
mxnctblt <thibaultmaxence44@gmail.com>
2022-11-22 00:11:05 -0800

    task 3 test

---
+++ file_io/3-cp.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+ * main - copy a file
+ * @argc: the number of args
+ * @argv: the args
+ * Return: exit_success if success, exit error 97, 98, 99, 100
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to;
+	int r = 1024, w = 0;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY | O_APPEND);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(file_from), exit(99);
+	}
+
+	while (r == 1024)
+	{
+		r = read(file_from, buffer, 1024);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		w = write(file_to, buffer, r);
+		if (w < r)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+
+	if (close(file_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+
+	return (0);
+}
