5b673b1c3d09d98994bcd05ecfad767248881bdc
mxnctblt <thibaultmaxence44@gmail.com>
2022-11-24 02:53:27 -0800

    task 8 test2

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -8,27 +8,34 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *new, *cpy = *head;
+	dlistint_t *cpy = *head;
 	unsigned int i = 0;
 
-	if (cpy == NULL)
+	if (*head == NULL)
 		return (-1);
+	while (cpy != NULL)
+	{
+		i++;
+		cpy = cpy->next;
+	}
+	if (i < index + 1)
+		return (-1);
+
+	cpy = *head;
 	if (index == 0)
 	{
-		*head = (*head)->next;
+		*head = cpy->next;
+		if (cpy->next)
+			cpy->next->prev = NULL;
+		cpy->next = NULL;
 		free(cpy);
 		return (1);
 	}
-	for (i = 0; i < index - 1; i++)
-	{
-		if (cpy->next == NULL)
-			return (-1);
+	while (index--)
 		cpy = cpy->next;
-	}
-
-	new = cpy->next;
-	cpy->next = new->next;
-	free(new);
-
+	cpy->prev->next = cpy->next;
+	if (cpy->next)
+		cpy->next->prev = cpy->prev;
+	free(cpy);
 	return (1);
 }
